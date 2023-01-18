49836f67f4bb740050421a6cc187113084de9cad
Mohammad <mohammadjaffal48@gmail.com>
2023-01-18 20:08:02 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Box} from '@mui/material'
 import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      HELLO
-    </div>
-  );
-}
+    <Router>
+      <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </Router>
+  )
+};
 
 export default App;
