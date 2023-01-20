f09d731115222d596a0a2922ea8758de133e7061
Mohammad <mohammadjaffal48@gmail.com>
2023-01-20 20:27:25 +0200

    Final

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,14 @@ export const exerciseOptions = {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     },
 };
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
   
 export const fetchData = async (url, options) => {
     const res = await fetch(url, options);
